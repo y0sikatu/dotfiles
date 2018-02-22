41afb130bc4c5419282b9102b2e5130bd4d8dabd
y0sikatu <y.iwasaki.y@gmail.com>
2018-02-22 23:45:45 +0900

    update init.el

--- init.el
+++ init.el
@@ -99,7 +99,23 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 ;; helm
+(require 'helm)
 (require 'helm-config)
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+(helm-autoresize-mode 1)
 (helm-mode 1)
 
 ;; irony
